e09ae40b0817ed5ab88907fbd3e5a4959182cddb
Dmitriy <larkingit@yandex.ru>
2019-12-24 14:26:07 +0700

    ДЗ_15 ч1

---
+++ lesson15/src/main/java/task01/DbManagerApp.java
@@ -0,0 +1,59 @@
+package task01;
+
+
+import java.sql.*;
+
+/**
+ *  Спроектировать базу
+ *Таблица USER содержит поля id, name, birthday, login_ID, city, email, description
+ *Таблица ROLE содержит поля id, name (принимает значения Administration, Clients, Billing), description
+ *Таблица USER_ROLE содержит поля id, user_id, role_id
+ *
+ *
+ *
+ */
+public class DbManagerApp {
+    public static volatile DbManagerApp instance;
+
+    private DbManagerApp() {};
+
+    public static DbManagerApp getInstance() {
+        DbManagerApp localInstance = instance;
+        if (localInstance == null) {
+            synchronized (DbManagerApp.class) {
+                localInstance = instance;
+                if (localInstance == null) {
+                    instance = localInstance = new DbManagerApp();
+                }
+            }
+        }
+        return localInstance;
+    }
+
+
+    public void createTables() throws ClassNotFoundException, IllegalAccessException, InstantiationException, SQLException {
+        Class.forName("org.h2.Driver").newInstance();
+        Connection connection = DriverManager.getConnection("jdbc:h2:mem:default");
+        connection.setAutoCommit(false);
+        String SQL = "CREATE TABLE IF NOT EXISTS USER (id INTEGER PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255) NOT NULL, birthday DATE, login_ID INT NOT NULL, city VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, description VARCHAR(Integer.MAX_VALUE))";
+        PreparedStatement ps = connection.prepareStatement(SQL);
+        ps.addBatch();
+        SQL = "CREATE TABLE IF NOT EXISTS ROLE(id INTEGER PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255) NOT NULL, description VARCHAR(Integer.MAX_VALUE) ADD CONSTRAINT THIS_ID_USER_ID FOREIGN KEY id REFERENCES USER.id)";
+        ps = connection.prepareStatement(SQL);
+        ps.addBatch();
+        SQL = "CREATE TABLE IF NOT EXISTS USER_ROLE(id INTEGER PRIMARY_KEY AUTO_INCREMENT, user_id INTEGER NOT NULL,  CONSTRAINT USER_id, role_id INT ADD CONSTRAINT THIS_ID_ROLE_ID FOREIGN KEY id REFERENCES USER_ROLE.id)";
+        ps = connection.prepareStatement(SQL);
+        ps.addBatch();
+        ps.executeBatch();
+    }
+
+
+
+    public static void main(String[] args) {
+        try {
+            DbManagerApp.getInstance().createTables();
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
