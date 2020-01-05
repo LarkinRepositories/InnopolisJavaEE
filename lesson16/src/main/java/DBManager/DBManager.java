413fc2e3273a71a053f5d566b09e17a24059a20f
Dmitriy <larkingit@yandex.ru>
2020-01-05 14:38:49 +0700

    исправил метод создания таблиц в бд

--- lesson16/src/main/java/DBManager/DBManager.java
+++ lesson16/src/main/java/DBManager/DBManager.java
@@ -3,10 +3,7 @@ package DBManager;
 import Logging.LoggingUtil;
 import org.apache.log4j.Logger;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DBManager {
     private static volatile DBManager instance;
@@ -39,8 +36,8 @@ public class DBManager {
         try {
             LOGGER.info("Trying to connect...");
             Class.forName("org.h2.Driver").newInstance();
-            connection = DriverManager.getConnection("jdbc:h2:./src/main/resources/lesson16.mv.db");
-            LOGGER.info("Connection successful");
+            connection = DriverManager.getConnection("jdbc:h2:./src/main/resources/lesson16_1.mv.db");
+            if (connection != null)  LOGGER.info("Connection successful");
         } catch (InstantiationException | SQLException | ClassNotFoundException | IllegalAccessException e) {
             e.printStackTrace();
             LOGGER.error("Connection ends up with ERROR: " + e);
@@ -69,30 +66,22 @@ public class DBManager {
     /**
      * Метод, создающий таблицы в БД
      */
-    public void createTables() {
+    public void createTables(Connection connection) {
         LOGGER.info("createTables method begins to work");
-        Connection connection = getConnection();
         try {
             connection.setAutoCommit(false);
             String SQL = "CREATE TABLE IF NOT EXISTS USER (id INTEGER PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255) NOT NULL, birthday DATE, login_ID INTEGER NOT NULL UNIQUE, city VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL UNIQUE, description VARCHAR(255))";
-            PreparedStatement ps = connection.prepareStatement(SQL);
-            ps.addBatch();
-            SQL = "CREATE TYPE ROLE as ENUM ('Administration', 'Clients', 'Billing')";
-            ps = connection.prepareStatement(SQL);
-            ps.addBatch();
-            ps.executeBatch();
-            SQL = "CREATE TABLE IF NOT EXISTS ROLE(id INTEGER PRIMARY KEY AUTO_INCREMENT, name ROLE, description VARCHAR(255))";
-            ps = connection.prepareStatement(SQL);
-            ps.addBatch();
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(SQL);
+            SQL = "CREATE TYPE IF NOT EXISTS ROLE as ENUM ('Administration', 'Clients', 'Billing')";
+            statement.executeUpdate(SQL);
+            SQL = "CREATE TABLE IF NOT EXISTS ROLE (id INTEGER PRIMARY KEY AUTO_INCREMENT, name ROLE, description VARCHAR(255))";
+            statement.executeUpdate(SQL);
             SQL = "CREATE TABLE IF NOT EXISTS USER_ROLE(id INTEGER PRIMARY KEY AUTO_INCREMENT, user_id INTEGER NOT NULL UNIQUE, role_id INTEGER NOT NULL)";
-            ps = connection.prepareStatement(SQL);
-            ps.addBatch();
+            statement.executeUpdate(SQL);
             SQL = "CREATE TABLE IF NOT EXISTS LOGS (id INTEGER PRIMARY KEY AUTO_INCREMENT, date DATE, log_level VARCHAR(5), message VARCHAR(255), exception VARCHAR(200))";
-            ps = connection.prepareStatement(SQL);
-            ps.addBatch();
-            ps.executeBatch();
+            statement.executeUpdate(SQL);
             LOGGER.info("tables successfully created");
-            System.out.println("TABLES SUCCESSFULLY CREATED");
         } catch (SQLException e) {
             e.printStackTrace();
             LOGGER.error("ERROR in createTables method:" + e);
